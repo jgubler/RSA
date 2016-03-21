703fa617477d993a8516cc735e2fb2d933277a29
jgubler <julian@juliangubler.de>
2016-03-20 18:49:04 -0700

    Refined fermat primality test, added gcd
    The fermat test no longer just uses an array of primes as coprimes but generates new ones instead.
    For this to work, a gcd fuction using the Euclidean algorithm was added.

--- keygen.c
+++ keygen.c
@@ -29,47 +29,64 @@ int chooseprime() {
 }
 
 // does several tests to approximate whether p is probably prime
-int testprime(int p) {
-    if(p==1 || p%2==0) return 1;
-    if(fermat(p)!=0) return 1;
+// (1 and 2 are excluded primes)
+int testprime(int potPrime) {
+    if(potPrime == 1 || potPrime%2 == 0) return 1;
+    if(fermat(potPrime, 15) != 0) return 1;
     return 0;
 }
 
 
-// tests if a number x is prime according to fermat's theorem, which 
-// states that if x is a prime number and a is any number for which
+// tests if potPrime is prime according to fermat's theorem, which 
+// states that if b is a prime number and a is any number for which
 // gcd(a,b) = 1 holds, a^(b-1) mod b = 1 must be true.
-// this function tests the property for the bases {3,5,7,11,13}
-// TODO: Optimize exponentiation
-int fermat(int b) {
-    int bases[5] = {3,5,7,11,13};
+// this function tests the property as many times as precision specifies
+int fermat(int potPrime, int precision) {
     long long res = 1;
-    for(int i=0;i<sizeof(bases)/sizeof(bases[0]);i++) {   
-        if(exponentialmod(bases[i], b) != 1) 
+    int coprime = 0;
+    for(int i=0; i<precision; i++) {
+        while(coprime == 0) {
+            coprime = rand();
+            if(gcd(potPrime, coprime) != 1) {
+                coprime = 0;
+            }            
+        }
+        if(modularExp(coprime, potPrime-1, potPrime) != 1) 
             return 1;
     }
     return 0;
 }
 
-// Implements a^(b-1) mod b, with fast exponentiation through square&multiply
-int exponentialmod(int a, int b) {
-    // log2(b-1)
-    int explog = log(b-1)/log(2);
+// Implements a^b mod n, with fast exponentiation through square&multiply
+int modularExp(int a, int b, int n) {
+    // log2(b)
+    int explog = log(b)/log(2);
     long long res = a;
-    int currentbit = 0;
-    // traverse the bits of b-1, square for every bit and if it is 1, multiply as well
-    for(int i=explog-1;i>=0;i--) {
+    int currentBit = 0;
+    // traverse the bits of b, square for every bit and if it is 1, multiply as well
+    for(int i=explog-1; i>=0; i--) {
         res *= res;
         // to get the bit for the current SAM cycle, we need to right shift by i and extract the last bit
-        currentbit = ((b-1)>>i)%2;
-        if(currentbit==1) res *=a;
-        res %= b;
+        currentBit = (b>>i) % 2;
+        if(currentBit==1) res *=a;
+        res %= n;
     }
     return res;
 }
 
 // Approximates whether x is a prime by calculating the miller-rabin test
 // TODO: Actually make it do something
-int miller(int x) {
+int miller(int potPrime) {
     return 0;
+}
+
+// Compute gcd with Euclidean algorithm
+int gcd (int pa, int pb) {
+    int a = pa, b = pb, c = 0;
+    while(a != 0) {
+        c = a;
+        a = b%a;
+        b = c;
+    }
+    return b;
 }
--- keygen.h
+++ keygen.h
@@ -2,9 +2,10 @@
 #define KEYGEN_H_
 
 int chooseprime();
-int exponentialmod(int,int);
 int testprime(int);
-int fermat(int);
+int fermat(int,int);
+int modularExp(int,int,int);
 int miller(int);
+int gcd(int,int);
 
 #endif // KEYGEN_H_
