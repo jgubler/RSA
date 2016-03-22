a49c8e3dbb0020924a8347faebe62689f34c16b9
jgubler <julian@juliangubler.de>
2016-03-22 13:41:00 -0700

    Added rsa.c, rsa.h (dummy), modified .gitignore
    Added rsa.c and rsa.h for the main part of the RSA algorithm, encryption and decryption.
    No functionality for encryption/decryption implemented yet, only parameter handling and
    usage output.

---
+++ rsa.c
@@ -0,0 +1,45 @@
+#include "rsa.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+	if(argc != 4) {
+		printUsage();
+		return 1;
+	}
+	int res;
+	// For now, only works with key files named key.pub/key.pr, see usage
+	if(strncmp(argv[1], "-e", 2) == 0 && strncmp(argv[2], "key.pub", 7) == 0) {
+		res = encrypt(argv[3], argv[2]);
+	} else if (strncmp(argv[2], "-e", 2) == 0 && strncmp(argv[3], "key.pub", 7) == 0) {
+		res = encrypt(argv[1], argv[3]);
+	} else if (strncmp(argv[1], "-d", 2) == 0 && strncmp(argv[2], "key.pr", 6) == 0) {
+		res = decrypt(argv[3], argv[2]);
+	} else if (strncmp(argv[2], "-d", 2) && strncmp(argv[3], "key.pr", 6) == 0) {
+		res = decrypt(argv[1], argv[3]);
+	} else {
+		printUsage();
+		return 1;
+	}
+	if(res != 0) {
+		fprintf(stderr, "Error Reading Files");
+	}
+	return res;
+}
+
+void printUsage() {
+	printf("\n\nPlease use one of the following usages: \n\n");
+	printf("\trsa <message file> -e key.pub\n");
+	printf("\trsa -e key.pub <message file>\n");
+	printf("\trsa <message file> -d key.pr\n");	
+	printf("\trsa -d key.pr <message file>\n\n\n");
+}
+
+int encrypt() {
+	return 0;
+}
+
+int decrypt() {
+	return 0;
+}
---
+++ rsa.h
@@ -0,0 +1,8 @@
+#ifndef RSA_H_
+#define RSA_H_
+
+void printUsage();
+int encrypt();
+int decrypt();
+
+#endif // RSA_H_
