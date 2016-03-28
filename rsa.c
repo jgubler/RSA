3d4af21124f3313bfcb4e6f09f9e7f20b55269ab
jgubler <julian@juliangubler.de>
2016-03-28 10:07:38 -0700

    RSA -e now produces an enrypted file "zekuremessage"

--- rsa.c
+++ rsa.c
@@ -1,8 +1,10 @@
 #include "rsa.h"
+#include <asn1_lib.h>
+#include <PubKey.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <asn1_lib.h>
+#include <math.h>
 
 int main(int argc, char* argv[]) {
 	if(argc != 4) {
@@ -37,14 +39,95 @@ void printUsage() {
 	printf("\trsa -d key.pr <message file>\n\n\n");
 }
 
-int encrypt() {
+int encrypt(char* message, char* key) {
 	printf("Can't encrypt yet, but here's the decoded public key:\n");
-	pubkey_decode("key.pub");
+	pubkey_decode(key);
+	printf("\nAnd here's the message: \n");
+	printFile(message);
+
+	printf("Trying to encrypt here, experimental\n");
+	printf("Message is encrypted in 24 Bit (3 Byte) blocks for now (:D)\n");
+
+	FILE *clearText, *cipherText;
+	clearText = fopen(message, "rb");
+	cipherText = fopen("zekuremessage","wb");
+	
+	PubKey_t* pubKey = pubkey_decode(key);
+	uint32_t pubExp, mod;
+    long temp = 0;
+    asn_INTEGER2long(&(pubKey->publicExponent), &temp);
+    pubExp = (uint32_t) temp;
+    asn_INTEGER2long(&(pubKey->modulus), &temp);
+    mod = (uint32_t) temp;
+
+	if(clearText && cipherText) {
+		int x=1, i=0, eof=0;
+		uint8_t bytes[3];
+		uint32_t clear=0, cipher=0;
+
+		while(!feof(clearText)) {
+			size_t availableBytes = fread((void*) bytes, 1, 3, clearText);
+			if(availableBytes == 1) {
+				clear += bytes[0];
+			} else if(availableBytes == 2) {
+				clear += (bytes[0] << 8) + bytes[1];
+			} else if(availableBytes ==3) {
+				clear += (bytes[0] << 16) + (bytes[1] << 8) + bytes[2];
+			}
+			uint32_t cipher = modularExp(clear, pubExp, mod);
+			fwrite(&cipher, 4, 1, cipherText);
+		}
+		if(clearText) {
+			fclose(clearText);
+		}
+		if(cipherText) {
+			fclose(cipherText);
+		} 
+	} else {
+		fprintf(stderr, "Error opening files\n");
+	}
+	
+
 	return 0;
 }
 
-int decrypt() {
+int decrypt(char* message, char* key) {
 	printf("Can't decrypt yet, but here's the decoded private key:\n");
-	prkey_decode("key.pr");
+	prkey_decode(key);
+	printf("\nAnd here's the message: \n");
+	printFile(message);
 	return 0;
+}
+
+// Implements a^b mod n, with fast exponentiation through square&multiply
+uint32_t modularExp (uint32_t a, uint32_t b, uint32_t n) {
+    // log2(b) = ln(b)/ln(2)
+    int explog = log(b)/log(2);
+    uint64_t res = a;
+    int currentBit = 0;
+    // traverse the bits of b, square for every bit and if it is 1, multiply as well
+    for(int i=explog-1; i>=0; i--) {
+        res = (res * res) % n;
+        // to get the bit for the current SAM cycle, we need to right shift by i and extract the last bit
+        currentBit = (b>>i) % 2;
+        if(currentBit==1) {
+            res = (res * a) % n;
+        }
+    }
+    return res;
+}
+
+void printFile(char* filename) {
+	int c;
+	FILE *file;
+	file = fopen(filename, "r");
+	if(file) {
+		while((c = getc(file)) != EOF) {
+			putchar(c);
+		}
+		fclose(file);
+		printf("\n");
+	} else {
+		fprintf(stderr, "Error reading file\n");
+	}
 }
--- rsa.h
+++ rsa.h
@@ -1,8 +1,11 @@
 #ifndef RSA_H_
 #define RSA_H_
+#include <stdint.h>
 
 void printUsage();
-int encrypt();
-int decrypt();
+int encrypt(char*,char*);
+int decrypt(char*,char*);
+uint32_t modularExp(uint32_t,uint32_t,uint32_t);
+void printFile(char*);
 
 #endif // RSA_H_
